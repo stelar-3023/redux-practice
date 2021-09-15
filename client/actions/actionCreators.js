7da7efdfcd75021652c5299b1e0cef72666e13b4
Steve Larsen <slarsen-3@att.net>
2021-09-15 17:52:46 -0500

    set up store and redux actions

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,33 @@
+// increment
+function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+  
+}
+// add comment
+function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+// remove comment
+function removeComment(postId, index) {
+  return {
+    type: 'REMOVE_COMMENT',
+    index,
+    postId
+  }
+}
+
+export {
+  increment,
+  addComment,
+  removeComment
+}
+
+// remove comment
