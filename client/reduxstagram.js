6c6dd169f61c73cda78b900f5f22c4fc2d50c76a
Steve Larsen <slarsen-3@att.net>
2021-09-15 16:34:02 -0500

    setup

---
+++ client/reduxstagram.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+// Import Components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
