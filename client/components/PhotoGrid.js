2291ff00af154b82f8107f4b3b4c6877a6142b21
Steve Larsen <slarsen-3@att.net>
2021-09-16 14:09:23 -0500

    updated PhotoGrid with map fucntion to import photo

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return <div className='photo-grid'>
-      I'm the photo-grid
+      {this.props.posts.map((post, i) => <Photo/>)}
     </div>;
   },
 });
